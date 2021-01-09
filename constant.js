163275daea75ba4116588728af71b235dea75b04
eity-islam <rimiislameity8@gmail.com>
2021-01-09 12:07:10 +0600

    add constant

---
+++ constant.js
@@ -0,0 +1,7 @@
+//var age = 12;
+let age = 34; //constant e opposite.
+age = 13;
+age = 17;
+console.log(age);       //output: 17
+const country = "bangladesh"; //fixed.man sorasori update korbo na.reassain korbo na
+console.log(country);   //output: bangladesh
